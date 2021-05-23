53607b6d66475a8ae73f4eaea29243c55d858a7b
Ishaanee28 <77971879+Ishaanee28@users.noreply.github.com>
2021-05-23 17:21:54 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,65 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visibility = 255;
+      World.add(world, this.body);
+      this.image = loadImage("sprites/block.png");
+    this.Visiblity = 255;
+    }
+    display(){
+      if(this.body.speed<3){
+        super.display();
+      }
+      else{
+        World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+      }
+      this.image=loadImage("block.png")
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+
+      
+    }
+}
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.Visiblity = 255;
+  }
+
+ display(){
+   //console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+   }
+  }
+
+  score(){
+    if (this.Visiblity < 0 && this.Visiblity > -1005){
+      score++;
+    }
+  }
+
+
+
+};
